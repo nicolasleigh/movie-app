6b6f58f36cea39e0038af9a835beffd05af6e9c7
NicolasLi <linze201@gmail.com>
2023-11-03 13:03:05 +0800

    add actorSearch

--- src/components/ActorSearch.tsx
+++ src/components/ActorSearch.tsx
@@ -6,7 +6,7 @@ import { Controller } from 'react-hook-form';
 import Select from 'react-select';
 
 let timeout: ReturnType<typeof setTimeout> | null;
-
+const arr = [];
 // const filterColors = (inputValue: string) => {
 //     return colourOptions.filter((i) =>
 //         i.label.toLowerCase().includes(inputValue.toLowerCase())
@@ -21,69 +21,92 @@ let timeout: ReturnType<typeof setTimeout> | null;
 //     });
 
 export default function ActorSearch({ name, control }) {
-    const [data, setData] = useState([]);
-    // const [options, setOptions] = useState([{}]);
+    const [inputValue, setValue] = useState('');
+    // const [data, setData] = useState([]);
+    const [options, setOptions] = useState([]);
 
-    const fetch = (e) => {
-        const { value } = e.target;
+    const fetch = (value) => {
         if (timeout) {
             clearTimeout(timeout);
             timeout = null;
         }
 
         const search = async () => {
-            const arr = [{}];
             const { results } = await searchActor(value);
-            const data = results.map((item: any) => {
+            results.map((item: any) => {
                 return arr.push({
                     value: item.avatar,
                     label: item.name,
                 });
             });
+            // setData(data);
             setOptions(arr);
-            setData(data);
-            console.log(arr);
-            console.log('data:', data);
+
+            // console.log('data:', data);
         };
         if (value) {
             timeout = setTimeout(search, 300);
         } else {
-            setData([]);
+            // setData([]);
         }
     };
 
-    const options = [
-        { value: '1', label: 'Apple' },
-        { value: '2', label: 'Ball' },
-        { value: '3', label: 'Cat' },
-    ];
+    // const options = [
+    //     { value: 'http://fake1.com', label: 'Jim Smith' },
+    //     { value: 'http://fake2.com', label: 'Ball Andy' },
+    //     { value: 'http://fake3.com', label: 'Cat Green' },
+    // ];
+
+    const loadOptions = async (inputValue, callback) => {
+        // fetch(inputValue);
+        // await searchActor(inputValue).then(({ results }) =>
+        //     results.map((e) => {
+        //         return arr.push({
+        //             value: e.avatar,
+        //             label: e.name,
+        //         });
+        //     })
+        // );
+        // setOptions(arr);
+        const { results } = await searchActor(inputValue);
+        callback(
+            results.map((e) => ({
+                label: e.name,
+                value: e.avatar,
+            }))
+        );
+    };
+
+    useEffect(() => {
+        console.log('arr:', arr);
+        console.log('options:', options);
+    }, [options]);
+
+    const handleInputChange = (value) => {
+        console.log('handleInputChange', value);
+        setValue(value);
+    };
 
     return (
         <Controller
             name={name}
-            defaultValue={options.map((c) => c.value)}
+            // defaultValue={options.map((c) => c.value)}
             control={control}
-            render={({ field: { onChange, value, ref } }) => (
-                <Select
+            render={({ field }) => (
+                <AsyncSelect
+                    {...field}
                     classNamePrefix='select'
-                    styles={{
-                        input: (base) => ({
-                            ...base,
-                            color: 'black',
-                            'input:focus': {
-                                boxShadow: 'none',
-                            },
-                        }),
-                    }}
                     // isMulti
                     // cacheOptions
                     // onChange={() => console.log('change')}
                     // options={options}
                     // defaultOptions
                     // loadOptions={promiseOptions}
-                    inputRef={ref}
-                    value={options.filter((c) => value.includes(c.value))}
-                    onChange={(val) => onChange(val.map((c) => c.value))}
+                    loadOptions={loadOptions}
+                    // onInputChange={handleInputChange}
+                    // inputRef={ref}
+                    // value={options.filter((c) => value.includes(c.value))}
+                    // onChange={(val) => onChange(val.map((c) => c.value))}
                     options={options}
                     isMulti
                 />
