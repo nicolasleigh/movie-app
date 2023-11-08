d9b0946defc05f06261f5c1b4d9119eb7b049974
NicolasLi <linze201@gmail.com>
2023-11-08 10:34:04 +0800

    use ReactPlayer

--- src/pages/SingleMovie.tsx
+++ src/pages/SingleMovie.tsx
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom';
 import { getSingleMovie } from '../api/movie';
 import { useToast } from '../hooks';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import VideoJS from '../components/VideoJS';
 import SideNav from '../components/SideNav';
+import ReactPlayer from 'react-player';
 
 export default function SingleMovie() {
     const playerRef = useRef(null);
@@ -48,51 +48,38 @@ export default function SingleMovie() {
     const videoName = video;
 
     const videoSrc = import.meta.env.VITE_MOVIE_BASE_URL + videoName;
+    const trackSrc =
+        import.meta.env.VITE_MOVIE_BASE_URL +
+        'House.of.Cards.S04E03.WEBRip.DEFLATE.en.vtt';
 
-    const videoJsOptions = {
-        autoplay: true,
-        controls: true,
-        responsive: true,
-        experimentalSvgIcons: true,
-        controlBar: {
-            skipButtons: {
-                forward: 5,
-                backward: 10,
-            },
+    const videoConfig = {
+        file: {
+            tracks: [
+                {
+                    kind: 'subtitles',
+                    src: trackSrc,
+                    srcLang: 'en',
+                    label: 'English',
+                    default: true,
+                    style: { backgroundColor: 'white' },
+                },
+            ],
+            attributes: { crossOrigin: 'true' },
         },
-
-        playbackRates: [0.5, 1, 1.5, 2],
-        language: 'zh-CN',
-        fluid: true,
-        sources: [
-            {
-                // src: 'http://localhost:8000/video/video-3835249a-ee87-477a-8d23-7b1acd35e18b.mp4',
-                src: videoSrc,
-                type: 'video/mp4',
-            },
-        ],
-        html5: {},
-    };
-
-    const handlePlayerReady = (player) => {
-        playerRef.current = player;
-
-        // You can handle player events here, for example:
-        player.on('waiting', () => {
-            videojs.log('player is waiting');
-        });
-
-        player.on('dispose', () => {
-            videojs.log('player will dispose');
-        });
     };
 
     return (
         <div className='flex'>
             <SideNav />
-            <div className='w-full h-full'>
-                <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-            </div>
+            <ReactPlayer
+                url={videoSrc}
+                controls={true}
+                width='60%'
+                height='60%'
+                playing={true}
+                muted={true} //temporary fix since videos with audio don't autoplay on most browsers
+                config={videoConfig}
+            />
         </div>
     );
 }
